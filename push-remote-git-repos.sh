d661c628bc88ab4e42abc6d6f4f82ede845dd1b4
qiushaocloud <qiushaocloud@126.com>
2024-01-08 01:37:04 +0800

    update

--- push-remote-git-repos.sh
+++ push-remote-git-repos.sh
@@ -12,32 +12,32 @@ echo "gitName: $gitName"
 # 推送到 gitee
 echo "推送到 gitee: git@gitee.com:qiushaocloud/$gitName"
 git remote set-url origin git@gitee.com:qiushaocloud/$gitName
-git push
+git push --set-upstream origin qtypings-browser-global
 
 # 推送到 github
 echo "推送到 github: git@github.com:qiushaocloud/$gitName"
 git remote set-url origin git@github.com:qiushaocloud/$gitName
-git push
+git push --set-upstream origin qtypings-browser-global
 
 # 推送到 gitlab
 echo "推送到 gitlab: git@gitlab.com:qiushaoyumeng/$gitName"
 git remote set-url origin git@gitlab.com:qiushaoyumeng/$gitName
-git push
+git push --set-upstream origin qtypings-browser-global
 
 # 推送到 gitcode
 echo "推送到 gitcode: git@gitcode.net:qiushaocloud/$gitName"
 git remote set-url origin git@gitcode.net:qiushaocloud/$gitName
-git push
+git push --set-upstream origin qtypings-browser-global
 
 # 推送到自建的 gitea
 # echo "推送到自建的 gitea: ssh://git@gitea.qiushaocloud.top:61322/qiushaocloud/$gitName"
 # git remote set-url origin ssh://git@gitea.qiushaocloud.top:61322/qiushaocloud/$gitName
-# git push
+# git push --set-upstream origin qtypings-browser-global
 
 # 推送到自建的 gitlab
 echo "推送到自建的 gitlab: ssh://git@gitlab.qiushaocloud.top:61023/qiushaocloud/$gitName"
 git remote set-url origin ssh://git@gitlab.qiushaocloud.top:61023/qiushaocloud/$gitName
-git push
+git push --set-upstream origin qtypings-browser-global
 
 echo "mv config"
 mv $CURR_DIR/.git/config.bak $CURR_DIR/.git/config
